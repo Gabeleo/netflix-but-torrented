094c0dd1ddbb5391448c4c12739aa111cf0c7a84
Gabriel Leo <gabe.morias@gmail.com>
2019-06-23 02:15:41 -0400

    fixed __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,16 +4,16 @@ from depend import htpc
 
 app = Flask(__name__)
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    @app.route('/result', methods=['GET', 'POST'])
-    def result():
-        if request.method == 'POST':
-            query = request.form['text']
-            htpc.create("https://thepiratebay.org/", query)
-            return redirect(htpc.infohash)
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        query = request.form['text']
+        htpc.create("https://thepiratebay.org/", query)
+        return redirect(htpc.infohash)
 
 if __name__=='__main__':
     app.run(debug=True)
