ca9e87ca31a4a50d81423391f1d5863c9ca965af
Gabriel Leo <gabe.morias@gmail.com>
2019-06-23 02:12:16 -0400

    rewrote __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,28 +2,7 @@ import os
 from flask import Flask, redirect, request, render_template
 from depend import htpc
 
-
-
-def create_app(test_config=None):
-    """Creates and configures the Flask application."""
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+app = Flask(__name__)
 
     @app.route('/')
     def index():
@@ -36,4 +15,5 @@ def create_app(test_config=None):
             htpc.create("https://thepiratebay.org/", query)
             return redirect(htpc.infohash)
 
-    return app
+if __name__=='__main__':
+    app.run(debug=True)
